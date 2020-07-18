e4637d146423202899cf5f3d077066b62b25c8d5
craigh210 <craighunt210@gmail.com>
2020-07-18 17:52:37 -0400

    Initial update

---
+++ SpringB204/src/test/java/edu/mc/chunt/SpringB204ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mc.chunt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringB204ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
